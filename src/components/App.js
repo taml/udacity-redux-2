70c312c0f676e74010c6ca5edc11c16dc13b13cf
Tam Love <dark_angel4eva@email.com>
2021-05-19 16:57:45 +0100

    Added New Tweet component and functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
@@ -12,7 +14,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true ? null : <TweetPage match={{params: {id: "3km0v4hf1ps92ajf4z2ytg"}}} />}
       </div>
     )
   }
